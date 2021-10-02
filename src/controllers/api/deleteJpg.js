3517c6edbe26c0a3a4bcb8b05c49edff94b2d877
KRASAVA <vladimirova.a-a@yandex.ru>
2021-10-02 18:53:37 +0400

    Add check for exist in db

--- src/controllers/api/deleteJpg.js
+++ src/controllers/api/deleteJpg.js
@@ -4,10 +4,15 @@ const {jpgFolder} = require('../../config');
 const db = require('../../entities/Database');
 const {removeFile} = require('../../utils/fs');
 const {createFileName} = require("../../utils/createFileName");
+const {NotFoundApiError} = require("../../validators/errors/ApiError");
 
 module.exports = async (req, res, next) => {
     try {
         const jpgId = req.params.id;
+
+        if (!db.findOne(jpgId)) {
+            throw new NotFoundApiError('JPG file not found');
+        }
 
         const fileName = createFileName(jpgId);
         const pathToFile = path.resolve(jpgFolder, fileName);
