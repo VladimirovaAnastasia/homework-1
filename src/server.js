4968e06a6d3037a0c8f5c22255e872bd9308a371
KRASAVA <vladimirova.a-a@yandex.ru>
2021-10-04 21:14:53 +0400

    Remove extra setting for convert request body at POST

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const router = require("./routers");
